4d38fcd1388efe64c12bcc8ba9588344ab34afba
maksi <maksimlya@gmail.com>
2018-11-25 09:44:53 +0200

    More bug fixes with p2p

--- p2p/handlers/Handlers.go
+++ p2p/handlers/Handlers.go
@@ -231,25 +231,26 @@ func HandleBlock(request []byte, chain *blockchain.Blockchain) {
 				nc.SendGetBlocks(node)
 			}
 		}
-	}
-
-	fmt.Println("Received a new Block!")
-	valid := chain.AddBlock(block)
-
-	if valid {
-		fmt.Printf("Added block %s\n", block.GetHash())
 	} else {
-		fmt.Println("Failed to add block.." + block.GetHash())
-	}
 
-	if len(blocksInTransit) > 0 {
-		blockHash := blocksInTransit[0]
-		nc.SendGetData(payload.AddrFrom, "block", blockHash)
+		fmt.Println("Received a new Block!")
+		valid := chain.AddBlock(block)
 
-		blocksInTransit = blocksInTransit[1:]
-	} else {
-		checkChain := chain.ValidateChain()
-		fmt.Println(checkChain)
+		if valid {
+			fmt.Printf("Added block %s\n", block.GetHash())
+		} else {
+			fmt.Println("Failed to add block.." + block.GetHash())
+		}
+
+		if len(blocksInTransit) > 0 {
+			blockHash := blocksInTransit[0]
+			nc.SendGetData(payload.AddrFrom, "block", blockHash)
+
+			blocksInTransit = blocksInTransit[1:]
+		} else {
+			checkChain := chain.ValidateChain()
+			fmt.Println(checkChain)
+		}
 	}
 
 }
